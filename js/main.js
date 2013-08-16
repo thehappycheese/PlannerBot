9ddc0088c1d53b11dd0e994c23a88a73db657915
Nicholas Archer <the.nicholas.archer@gmail.com>
2013-08-17 02:05:59 +0800

    Hacked in an iso-spritemaker

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 ///* ../lib/Canvas.js
 ///* ../lib/Sprite.js
+///* ../lib/Math.js
 ///* World.js
 
 
@@ -13,9 +14,40 @@ var res = {i:{}, a:{}};
 res.i.wall = new Sprite("img/wall4.png", 4, 4);
 res.i.grass = new Sprite("img/grass.png", 1, 1);
 res.i.player = new Sprite("img/robot2.png", 4, 2);
-res.i.star = new Sprite("img/star2.png", 1, 1);
+//res.i.star = new Sprite("img/star2.png", 1, 1);
 
+// =======================================================================
 
+var scale = 8;
+function draw(){
+	var x,y,z;
+	var xd,yd,zd;
+	var c = canvas.ctx;
+	c.fillStyle = "white";
+	c.fillRect(0,0,32*scale,32*scale);
+	for(y=0;y<32;y++){
+		for(x=0;x<32;x++){
+			for(z=0;z<32;z++){
+				xd = 16-x;
+				yd = 16-y;
+				zd = 16-z;
+				if(Math.sqrt(xd*xd+yd*yd+zd*zd)<16 || (zd>0 && Math.sqrt(xd*xd+yd*yd)<16)){
+					pix(x,y,z,"rgb("+(x*8)+","+(y*8)+","+(z*8)+")");
+				}
+			}
+		}
+	}
+}
+
+function pix(x,y,z,col){
+	canvas.ctx.fillStyle = col;
+	var pnt = {x:x*scale*Math.SQRT1_2, y:y*scale*Math.SQRT1_2};
+	Math.rotate(pnt,-Math.PI/4);
+	pnt.x = Math.round(pnt.x/scale)*scale;
+	pnt.y = Math.round(pnt.y/scale/1.99)*scale + 23*scale - Math.round(0.5*z)*scale;
+	canvas.ctx.fillRect(pnt.x, pnt.y,scale,scale);
+	
+}
 
 
 // =============================== INIT ===================================
@@ -56,7 +88,9 @@ canvas.on("update",function(delta){
 
 canvas.on("draw", function (ctx) {
 	canvas.clear();
-	world.draw(ctx);
+	
+	//world.draw(ctx);
+	draw();
 });
 
 
